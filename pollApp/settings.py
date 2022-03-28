adf90403efe373145da54a569eac4f565a107ba1
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-28 09:25:17 -0700

    setup database as postgresql

--- pollApp/settings.py
+++ pollApp/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'pollApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_polls',
+        'USER': 'jasonlutz',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
