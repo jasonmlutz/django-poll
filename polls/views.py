1f4f92e1e066030e32dc9c5fe24b1c7a82596c4d
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-28 14:20:19 -0700

    Add back-end handling of vote

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse, Http404
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 from polls.models import Question
@@ -9,7 +10,22 @@ from .models import Question
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question, 'id': question_id})
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with post data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    # return render(request, 'polls/detail.html', {'question': question, 'id': question_id})
 
 
 def results(request, question_id):
@@ -18,7 +34,8 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    # return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
 
 
 def index(request):
