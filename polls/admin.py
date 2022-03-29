44271387dcef87eccddc1b5e0f2d481efc6bfc8f
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-29 11:38:42 -0700

    Register choices to admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 # Register your models here.
 
 
@@ -11,3 +11,4 @@ class QuestionAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
