a1e83cffcdc1dffcacd0fd1c8d2d8df29ad7f0f2
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-29 11:40:05 -0700

    Add ChoiceInLine

--- polls/admin.py
+++ polls/admin.py
@@ -3,12 +3,17 @@ from .models import Choice, Question
 # Register your models here.
 
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date Information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
