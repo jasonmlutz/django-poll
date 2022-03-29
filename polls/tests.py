184501d27eff07e3e207b0eb529f6a2301f727fd
Jason Lutz <jason.m.lutz@gmail.com>
2022-03-29 11:20:49 -0700

    Create detail view test_future_question

--- polls/tests.py
+++ polls/tests.py
@@ -20,6 +20,19 @@ def create_question(question_text, days):
     return Question.objects.create(question_text=question_text, pub_date=time)
 
 
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = create_question(
+            question_text="Future question", days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+
 class QuestionModelTests(TestCase):
     def test_no_questions(self):
         """
